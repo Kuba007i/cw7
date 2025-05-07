6b5fa9e77e56683432573ba655dbef3036458492
Kuba007i <48179125+Kuba007i@users.noreply.github.com>
2025-05-07 11:44:20 +0200

    Add files via upload

---
+++ PolitykDetailsGetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD_Test1_Example.DTOs;
+
+public class PolitykDetailsGetDto
+{
+    public int ID { get; set; }
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public string Powiedzenie { get; set; }
+    public List<PolitykPartiaGetDto> Partie { get; set; }
+}

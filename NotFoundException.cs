a155ba21b965968bc27c5c8e0297e4e2cb1afc13
Kuba007i <kuba00700@outlook.com>
2025-05-26 11:40:49 +0200

    Revert "Add files via upload"
    This reverts commit 6b5fa9e77e56683432573ba655dbef3036458492.

--- NotFoundException.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace APBD_Test1_Example.Exceptions;
-
-public class NotFoundException(string message) : Exception(message);
--- PolitykPartiaGetDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APBD_Test1_Example.DTOs;
-
-public class PolitykPartiaGetDto
-{
-    public int ID { get; set; }
-    public string Nazwa { get; set; }
-    public string? Skrot { get; set; }
-    public DateTime DataZalozenia { get; set; }
-}
